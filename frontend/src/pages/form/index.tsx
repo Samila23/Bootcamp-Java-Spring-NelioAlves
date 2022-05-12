421d0842a2996ce55a23f9ee5d2b27e1b13d12fb
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-05-12 14:50:02 -0300

    @Samila23 - Navigation buttons

--- frontend/src/pages/form/index.tsx
+++ frontend/src/pages/form/index.tsx
@@ -1,4 +1,6 @@
 import './style.css'
+import { Link } from "react-router-dom";
+
 function Form() {
   const movie = {
     id: 1,
@@ -35,7 +37,9 @@ function Form() {
             </button>
           </div>
         </form>
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        <Link to ="/">
+          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        </Link>
       </div>
     </div>
   );
