fb09adeec4c113fe78c049de4054714293881d66
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-05-12 11:43:43 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
-import Navbar from './components/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/listing';
+import Form from 'pages/form';
+import Navbar from "components/Navbar";
+
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
