302ec2bec0b58d14c8b32f99bcce9a34c42e9996
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-05-12 09:26:22 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/devsuperior/dsmovie/DsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
