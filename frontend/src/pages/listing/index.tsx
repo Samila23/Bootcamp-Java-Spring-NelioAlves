2d4437eaa9936b505aea1c441ce5ab17275d7671
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-05-13 18:59:52 -0300

    @Samila23 - useState , useEffect

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,17 +1,30 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movies";
 import { BASE_URL } from "utils/requests";
 
 function Listing(){
+    const [pageNumber, setPageNumber] = useState(0);
 
+    useEffect(() =>{
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
+        });
+    }, [] );
     //Forma errada
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
-        console.log(response.data);
-    });
+    // axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    // .then(response => {
+    //     const data = response.data as MoviePage;
+    //     setPageNumber(data.number);
+    // });
     return(
         <>
+        <p>{pageNumber}</p>
             <Pagination/>
             <div className="container">
                 <div className="row">
