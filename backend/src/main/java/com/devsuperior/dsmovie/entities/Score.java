2dedc9d30a077c3093e56643b8abf32154af30fc
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-05-15 08:06:30 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -21,7 +21,7 @@ public class Score {
 	public void setMovie(Movie movie) {
 		id.setMovie(movie);
 	}
-	public void setUser(User user) {
+	public void setUser(User user){
 		id.setUser(user);
 	}
 	//Get e Set
--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,53 +1,54 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import { useEffect, useState } from "react";
-import { MoviePage } from "types/movies";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movies";
 
-function Listing(){
+function Listing() {
+
     const [pageNumber, setPageNumber] = useState(0);
 
     const [page, setPage] = useState<MoviePage>({
-            content: [],
-            last: true,
-            totalPages: 0,
-            totalElements: 0,
-            size: 12,
-            number: 0,
-            first: true,
-            numberOfElements: 0,
-            empty: true
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
     });
-    useEffect(() =>{
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
-        .then(response => {
-            const data = response.data as MoviePage;
-            setPage(data);
-        });
-    },  [pageNumber] );
-    //Forma errada
-    // axios.get(`${BASE_URL}/movies?size=12&page=1`)
-    // .then(response => {
-    //     const data = response.data as MoviePage;
-    //     setPageNumber(data.number);
-    // });
 
-    return(
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber]);
+
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+
+    return (
         <>
-            <Pagination/>
+            <Pagination page={page} onChange={handlePageChange} />
+
             <div className="container">
                 <div className="row">
-                    {page.content.map(movie =>
-                        (
-                            <div key={movie.id}  className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                                <MovieCard movie={movie}/>
-                            </div>
-                        )
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
                     )}
                 </div>
             </div>
         </>
-     )
+    );
 }
+
 export default Listing;
